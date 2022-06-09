4108e9b0ba36a0699163dda49487690555e522b5
sahaveer <sahavbachi@gmail.com>
2022-06-09 22:19:22 +0530

    changed code

---
+++ pages/01_About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown(""" <style> .font {
+font-size:22px ; font-family: 'Cooper Black'; color: #FF9633;} 
+</style> """, unsafe_allow_html=True)
+
+st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)
+st.write("We @iTimes are trying to create basic DIY fundamental analysis. \n\n We shall try bringing you here bse announcements, news, amibroker eod data here")
