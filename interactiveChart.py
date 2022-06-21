8d02eaf17c56b2036f05d9c71d7fe6f08e8f3f85
sahaveer <sahavbachi@gmail.com>
2022-06-21 06:35:05 +0530

    kept code simple

--- interactiveChart.py
+++ interactiveChart.py
@@ -14,11 +14,13 @@ def main():
     st.markdown(""" <style> .font1 {
                 font-size:50px ; font-family: 'Copper Black'; color: seablue;} 
                 </style> """, unsafe_allow_html=True)
-    col1, col2 = st.columns([0.8, 0.2])
-    with col1:
-        st.markdown('<p class="font1">iTimesAlgo</p>', unsafe_allow_html=True)
-    with col2:  # To display brand log
-        st.image(logo, width=80)
+    st.markdown('<p class="font1">iTimesAlgo</p>', unsafe_allow_html=True)
+
+    #col1, col2 = st.columns([0.8, 0.2])
+    #with col1:
+        #st.markdown('<p class="font1">iTimesAlgo</p>', unsafe_allow_html=True)
+    #with col2:  # To display brand log
+        #st.image(logo, width=80)
 
     st.markdown(""" <style> .font {
     font-size:22px ; font-family: 'Cooper Black'; color: #FF9633;} 
@@ -26,7 +28,23 @@ def main():
 
     st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)
     st.markdown("---")
-    st.write("We @iTimes are trying to create basic DIY fundamental analysis. \n\n We shall try bringing you here bse announcements, news, amibroker eod data here")
+    st.write("We @iTimes are trying to create basic DIY fundamental analysis. \n\n We shall try bringing you here bse announcements and amibroker eod data here")
+
+    st.markdown(""" <style> .font {font-size:36px ; font-family: 'Cooper Black'; color: #FF9633;} </style> """,
+                unsafe_allow_html=True)
+
+    col1, col2 = st.columns([0.8, 0.2])
+    with col1:  # To display the header text using css style
+        st.markdown('<p class="font">Contact us through</p>', unsafe_allow_html=True)
+        st.markdown("[telegram](https://t.me/itimesalgo/)")
+        st.markdown("[Twitter](https://twitter.com/itimesalgo)")
+        # st.markdown('<p class="font">Contact us through [telegram](https://t.me/itimesalgo/). </p>', unsafe_allow_html=True)
+
+    with col2:  # To display brand log
+        st.image(logo, width=130)
+
+    st.write("We sincerely appreciates your suggestions and contribution to improvise our iTimes community.")
+
 
 if __name__ == '__main__':
     main()
