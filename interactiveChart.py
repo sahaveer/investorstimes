3a812df0f3d511414bbd8fca15cb33a63814fd38
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2022-08-17 23:39:26 +0530

    added tradingview widget

--- interactiveChart.py
+++ interactiveChart.py
@@ -2,30 +2,34 @@ import streamlit as st
 import glob
 import pandas as pd
 import fundamentals
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as components
 
+st.set_page_config(page_title="iTimesAlgo", page_icon=":bar_chart:", layout="wide",initial_sidebar_state="expanded",)
+funda_keys = ['PROFIT&LOSS', 'BALANCE SHEET',
+              'CASH FLOW']  # dont change the order of this list as it will affect the keys used in Yearly df
+# **************************************************************************************************
+listed_stocks = []
+stocks_dict = {}
+color_dict = {'Yellow_Lite': "#f8ba43", 'Yellow_Dark': "#D6D41B", 'Blue_Lite': "#0FBAEC", 'Blue_Dark': "#0971C9",
+              'Green_Lite': "#11A694", 'Green_Dark': "#11A64B", "Purple_Lite": "#7019BF", 'Purple_Dark': "#9319BF"}
+# color_list = ["#D6D41B","#f8ba43","#0971C9","#1959BF","#11A694","#11A64B","#7019BF","#9319BF"]
+color_line = "Red"
+for each_pickl in glob.glob('./pickl/*.pkl', recursive=False):
+    each_pickl = each_pickl.replace('\\', '/')
+    file_name_only = each_pickl.split('/')[-1]
+    pickle_name = file_name_only.split('.pkl')[0]
+    stocks_dict[pickle_name] = each_pickl
+    listed_stocks += [pickle_name]
 
-#st.set_page_config(page_title="iTimesAlgo",page_icon=":bar_chart:",layout="wide")
-def main():
-    funda_keys = ['PROFIT&LOSS', 'BALANCE SHEET','CASH FLOW']  # dont change the order of this list as it will affect the keys used in Yearly df
-    # **************************************************************************************************
-    listed_stocks = []
-    stocks_dict = {}
-    color_dict = {'Yellow_Lite': "#f8ba43", 'Yellow_Dark': "#D6D41B", 'Blue_Lite': "#0FBAEC", 'Blue_Dark': "#0971C9",
-                  'Green_Lite': "#11A694", 'Green_Dark': "#11A64B", "Purple_Lite": "#7019BF", 'Purple_Dark': "#9319BF"}
-    # color_list = ["#D6D41B","#f8ba43","#0971C9","#1959BF","#11A694","#11A64B","#7019BF","#9319BF"]
-    color_line = "Red"
-    for each_pickl in glob.glob('./pickl/*.pkl', recursive=False):
-        each_pickl = each_pickl.replace('\\', '/')
-        file_name_only = each_pickl.split('/')[-1]
-        pickle_name = file_name_only.split('.pkl')[0]
-        stocks_dict[pickle_name] = each_pickl
-        listed_stocks += [pickle_name]
+with st.sidebar:
+    st.write(r"we have got now {} listed stocks data available".format(str(len(listed_stocks))))
+    selected = st.selectbox("NSE Listed", listed_stocks)
 
-    with st.sidebar:
-        st.write(r"we have got now {} listed stocks data available".format(str(len(listed_stocks))))
-        selected = st.selectbox("NSE Listed", listed_stocks)
-
-    if selected:
+if selected:
+    funda_tech = option_menu("", ["Funda_Chart", 'Tech_Chart'],
+                             icons=['house', '📈 '], menu_icon="cast", default_index=0, orientation="horizontal")
+    if funda_tech == "Funda_Chart":
         col1, col2, col3 = st.columns([0.5, 0.3, 0.2])
         with col1:
             st.subheader('📈 ' + selected)
@@ -39,7 +43,49 @@ def main():
         except Exception as AttributeError:
             pass
         comp_Name = str(selected)
-        sub_choose = st.sidebar.selectbox("Fundamentals", fundamentals.funda_keys)
+        sub_choose = st.sidebar.selectbox("Fundamentals", fundamentals.funda_keys,index=3)
+        if sub_choose == fundamentals.funda_keys[3]:
+            key_data = """<!-- TradingView Widget BEGIN -->
+                            <div class="tradingview-widget-container">
+                              <div class="tradingview-widget-container__widget"></div>
+                              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/financials-overview/" rel="noopener" target="_blank"><span class="blue-text">Fundamental Data</span></a> by TradingView</div>
+                              <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-financials.js" async>
+                              {
+                              "colorTheme": "dark",
+                              "isTransparent": false,
+                              "largeChartUrl": "",
+                              "displayMode": "regular",
+                              "width": "100%",
+                              "height": 880,
+                              "symbol": "balamines",
+                              "locale": "en"
+                            }
+                              </script>
+                            </div>
+                            <!-- TradingView Widget END -->"""
+
+            comp_profile = """<!-- TradingView Widget BEGIN -->
+                            <div class="tradingview-widget-container">
+                              <div class="tradingview-widget-container__widget"></div>
+                              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text"> Profile</span></a> by TradingView</div>
+                              <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-profile.js" async>
+                              {
+                              "width": "100%",
+                              "height": 880,
+                              "colorTheme": "dark",
+                              "isTransparent": false,
+                              "symbol": "AAPL",
+                              "locale": "en"
+                            }
+                              </script>
+                            </div>
+                            <!-- TradingView Widget END -->"""
+            col1,col2 = st.columns([0.7,0.3])
+            with col1:
+                components.html(key_data, height=1080)
+            with col2:
+                components.html(comp_profile, height=1080)
+
         if sub_choose == fundamentals.funda_keys[0]:
             index_list = ["key_params"] + list(df_comp.loc[sub_choose].index)
             param = st.sidebar.selectbox("Params", index_list)
@@ -92,14 +138,81 @@ def main():
                     fundamentals.qoq_growth(df_comp.loc[sub_choose], param, color_dict[color_key], comp_Name)
                 else:
                     fundamentals.go_bar(df_comp.loc[sub_choose], param, color_dict[color_key], comp_Name)
+    if funda_tech == "Tech_Chart":
+        tech_analytics_widget = """<!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+          <div id="analytics-platform-chart-demo"></div>
+          <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+          <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+          <script type="text/javascript">
+          new TradingView.widget(
+          {
+          "container_id": "analytics-platform-chart-demo",
+          "width": "100%","height": "680",
+          "symbol": "ADANIPORTS",
+          "interval": "W",
+          "timezone": "exchange",
+          "theme": "dark",
+          "style": "0",
+          "toolbar_bg": "#f1f3f6",
+          "withdateranges": true,
+          "allow_symbol_change": true,
+          "save_image": false,
+          "details": true,"hotlist": true,"calendar": true,
+          "studies": [
+            "RSI@tv-basicstudies",
+            "MASimple@tv-basicstudies",
+            "MASimple@tv-basicstudies"
+          ],
+          "show_popup_button": true,
+          "popup_width": "1000",
+          "popup_height": "650",
+          "locale": "en"
+        }
+          );
+          </script>
+        </div>
+        <!-- TradingView Widget END -->"""
+        components.html(tech_analytics_widget, height=1080)
+        tech_chart_widget = """<!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+          <div id="technical-analysis-chart-demo"></div>
+          <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+          <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+          <script type="text/javascript">
+          new TradingView.widget(
+          {
+          "container_id": "technical-analysis-chart-demo",
+          "width": "100%",
+          "height": "680",
+          "symbol": "nifty",
+          "interval": "D",
+          "timezone": "exchange",
+          "theme": "dark",
+          "style": "1",
+          "toolbar_bg": "#f1f3f6",
+          "withdateranges": true,
+          "hide_side_toolbar": false,
+          "allow_symbol_change": true,
+          "save_image": false,
+          "studies": [
+            "RSI@tv-basicstudies",
+            "MASimple@tv-basicstudies",
+            "MASimple@tv-basicstudies"
+          ],
+          "show_popup_button": true,
+          "popup_width": "1000",
+          "popup_height": "650",
+          "locale": "en"
+        }
+          );
+          </script>
+        </div>
+        <!-- TradingView Widget END -->"""
+        # components.html(tech_chart_widget, height=1080)
 
-    st.write("____")
-    st.write('made with :green_heart: to Indian Stock Investors')
-
-
-if __name__ == '__main__':
-    main()
-
+st.write("____")
+st.write('made with :green_heart: to Indian Stock Investors')
 
 #Custom CSS to remove header,footer, hamburger icon
 hide_st_style = """
