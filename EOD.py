6d2780c64354756468c8a255f67ff233de373bf7
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-02 17:21:46 +0530

    Update EOD.py

--- EOD.py
+++ EOD.py
@@ -288,47 +288,19 @@ def csv_download(full_link,possible_bhav_name):             # CSV files for NSE 
     try:
         csvbhav_path = './bhavfiles/' + possible_bhav_name
         opener = urllib.request.build_opener()
-        #opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36')]
-        opener.addheaders = {
-                            'authority': 'beta.nseindia.com',
-                            'cache-control': 'max-age=0',
-                            'dnt': '1',
-                            'upgrade-insecure-requests': '1',
-                            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-                            'sec-fetch-user': '?1',
-                            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                            'sec-fetch-site': 'none',
-                            'sec-fetch-mode': 'navigate',
-                            'accept-encoding': 'gzip, deflate, br',
-                            'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
-                            }
+        opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36')]
         urllib.request.install_opener(opener)
+        
         with urllib.request.urlopen(full_link, timeout=10) as test_file, open(csvbhav_path, 'w',newline="") as f:
             f.write(test_file.read().decode())
             #st.success(f"Downloaded CSV from NSE site in {bhav_csv_path}")
         return csvbhav_path
     except Exception as e:
-        try:
-            set_cookie()
-            headers = {
-                        'authority': 'beta.nseindia.com',
-                        'cache-control': 'max-age=0',
-                        'dnt': '1',
-                        'upgrade-insecure-requests': '1',
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-                        'sec-fetch-user': '?1',
-                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                        'sec-fetch-site': 'none',
-                        'sec-fetch-mode': 'navigate',
-                        'accept-encoding': 'gzip, deflate, br',
-                        'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
-                    }
-            with urllib.request.urlopen(url, headers=headers, timeout=15, cookies=cookies) as test_file, open(csvbhav_path, 'w',newline="") as f:
-                f.write(test_file.read().decode())
-        except :
-            pass
-            #st.error(f"Failed to download through headers as well")
+        urllib.request.urlretrieve(full_link, csvbhav_path)
         st.error(f"{full_link} failed due to {e} ")
+
+#def csv_from_other(nse_full_link,possible_fullbhav_name):
+    
 
 
 def nse_file(nse_full_link,possible_fullbhav_name,txt2_name):
