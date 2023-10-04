dfd08657e7618aca5ddc325aabaa3337ec02f09f
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-04 15:38:30 +0530

    Update EOD.py

--- EOD.py
+++ EOD.py
@@ -53,8 +53,8 @@ mongodb_password = secrets["mongo"]["password"]
 @st.cache_resource
 def init_connection():
     #Connection_String = "mongodb+srv://EODBhavcopy:bhavcopy@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
-    Connection_String = f"mongodb+srv://{mongodb_username}:{mongodb_password}@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
-    return MongoClient(Connection_String)
+    #Connection_String = f"mongodb+srv://{mongodb_username}:{mongodb_password}@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
+    return MongoClient(**st.secrets["mongo"])
 
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
