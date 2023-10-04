c0245294753f2793b89fde6eca683c3bb9be8ce0
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-04 22:17:59 +0530

    Update EOD.py

--- EOD.py
+++ EOD.py
@@ -65,11 +65,13 @@ st.write("External IP:", external_ip)
 
 #@st.cache_resource
 def init_connection():
-    #Connection_String = "mongodb+srv://EODBhavcopy:bhavcopy@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
     #Connection_String = f"mongodb+srv://{mongodb_username}:{mongodb_password}@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
-    return MongoClient(**st.secrets["mongo"])
+    #return MongoClient(**st.secrets["mongo"])
     #Connection_String = f"mongodb+srv://{st.secrets["mongo"]}@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
     #return MongoClient(Connection_String, server_api=ServerApi('1'), tls=True)
+    Connection_String = "mongodb+srv://EODBhavcopy:bhavcopy@eodbhavcopy.4tbvocy.mongodb.net/?retryWrites=true&w=majority"
+    return MongoClient(Connection_String)
+
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 #@st.cache_data(ttl=600)
 def mongo_data(client):
