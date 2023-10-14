6f4c90584de9dcd41678d4175c1f30f6c844307b
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-14 14:08:02 +0530

    Update fundamentals.py

--- fundamentals.py
+++ fundamentals.py
@@ -23,8 +23,8 @@ funda_menu = funda_keys + ['QTR PnL', 'Key_Data'] # used for showing the menu in
 color_hover = "darkgrey"
 color_background = "grey"
 #Top 16:9 Resolutions. 640 x 360 (nHD) 854 x 480 (FWVGA) 960 x 540 (qHD) 1024 x 576 (WSVGA) 1280 x 720 (HD/WXGA) 1366 x 768 (FWXGA) 1600 x 900 (HD+) 1920 x 1080 (FHD) 2048 x 1152 (QWXGA) 2560 x 1440 (QHD) 3200 x 1800 (WQXGA+) 3840 x 2160 (UHD) 5120 x 2880 (UHD+) 7680 x 4320 (FUHD) 15360 x 8640 (QUHD) 30720 x 17280 (HHD) 61440 x 34560 (FHHD) 122880 x 69120 (QHHD)
-height_val = 680 #574
-width_val = 1209 #1024
+#height_val = 680 #574
+#width_val = 1209 #1024
 width_val = 1120
 height_val = 360
 
