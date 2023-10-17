e214646f709988efa181149127f9f06961ce2240
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-17 13:18:34 +0530

    Update 04_Portfolio.py

--- pages/04_Portfolio.py
+++ pages/04_Portfolio.py
@@ -14,7 +14,7 @@ from datetime import timedelta
 st.set_page_config(page_title="Portfolio", page_icon=":bar_chart:", layout="wide",initial_sidebar_state="collapsed",)
 st.title('Visualise your Portfolio ')
 
-st.markdown("[Zerodha](https://console.zerodha.com/dashboardst)")    
+st.markdown("[Download Holdings from Zerodha](https://console.zerodha.com/dashboardst)")    
 tradebook = st.file_uploader("upload TradeBooks from ZERODHA", type= ['xlsx'],accept_multiple_files = True)
 
 # Get today's date
