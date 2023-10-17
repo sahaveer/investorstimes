e60bbf9cf49203556ddb3e83661491d6a66b0b74
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-17 12:31:23 +0530

    Update 04_Portfolio.py

--- pages/04_Portfolio.py
+++ pages/04_Portfolio.py
@@ -11,7 +11,7 @@ import lastdayprice
 import time
 import datetime
 from datetime import timedelta
-
+st.set_page_config(page_title="Portfolio", page_icon=":bar_chart:", layout="wide",initial_sidebar_state="collapsed",)
 st.title('Visualise your Portfolio ')
 tradebook = st.file_uploader("upload TradeBooks from ZERODHA", type= ['xlsx'],accept_multiple_files = True)
 
