57fbd5a9e12512289f253e5499eb86390b608675
Sahaveer Reddy <64600033+sahaveer@users.noreply.github.com>
2023-10-19 15:59:11 +0530

    Update 01_Upload.py
    adding extra columns

--- pages/01_Upload.py
+++ pages/01_Upload.py
@@ -12,9 +12,8 @@ import os
 import fundamentals
 import json
 from streamlit_lottie import st_lottie
-st.set_page_config(page_title="Upload", page_icon=":bar_chart:", layout="wide",initial_sidebar_state="collapsed",)
+st.set_page_config(page_title="Upload", page_icon=":bar_chart:", layout="wide",initial_sidebar_state="expanded",)
 st.title('Upload _Excel file_ from [***SCREENER***]({https://www.screener.in/}) ')
-uploaded_file = st.file_uploader("", type=['xlsx','xlsm'],accept_multiple_files = True)  # Only accepts xlsx,xlsm file format
 color_dict = {'Yellow_Lite':"#f8ba43",'Yellow_Dark':"#D6D41B",'Blue_Lite':"#1959BF",'Blue_Dark':"#0971C9",'Green_Lite':"#11A694",'Green_Dark':"#11A64B","Purple_Lite":"#7019BF",'Purple_Dark':"#9319BF"}
 #color_list = ["#D6D41B","#f8ba43","#0971C9","#1959BF","#11A694","#11A64B","#7019BF","#9319BF"]
 def load_lottiefile(filepath: str):
@@ -33,6 +32,7 @@ with st.sidebar:
     </style> """, unsafe_allow_html=True)
     # Add a file uploader to allow users to upload their csv file
 
+    uploaded_file = st.file_uploader("", type=['xlsx','xlsm'],accept_multiple_files = True)  # Only accepts xlsx,xlsm file format
     st.markdown('<p class="font">Upload One or Two xlsx/xlsm FILES from screener.in </p>',
                 unsafe_allow_html=True)
     st_lottie(
@@ -58,6 +58,50 @@ if uploaded_file is not None:
         book = openpyxl.load_workbook(uploaded_file[0])
         #comp_name = book['Data Sheet']['B1'].value
         qtr_pnl,df_comp = fundamentals.get_tables(book[fundamentals.tabs[-1]], uploaded_file[0])  # send a sheet(not whole workbook)
+
+        pnl = df_comp.loc['PROFIT&LOSS']
+        pnl.fillna(0, inplace=True)
+        pnl.index = pnl.index.str.strip()
+        pnl = pnl.transpose()
+        pnl['EXPENSES'] = pnl['RAW MATERIAL COST'] - pnl['CHANGE IN INVENTORY'] + pnl['POWER AND FUEL'] + pnl[
+            'OTHER MFR. EXP'] + pnl['EMPLOYEE COST'] + pnl['SELLING AND ADMIN'] + pnl['OTHER EXPENSES']
+        pnl = pnl.drop(
+            columns=['RAW MATERIAL COST', 'CHANGE IN INVENTORY', 'POWER AND FUEL', 'OTHER MFR. EXP', 'EMPLOYEE COST',
+                     'SELLING AND ADMIN', 'OTHER EXPENSES'], axis=1)
+        pnl['OPERATING PROFIT'] = pnl['SALES'] - pnl['EXPENSES']
+        pnl['OPM %'] = pnl.apply(fundamentals.OPM, axis=1)
+        pnl['NPM %'] = pnl.apply(fundamentals.NPM, axis=1)
+        # Calculate the QoQ percentage increase for SALES, NET PROFIT, and OPERATING PROFIT
+        pnl['SALES_QoQ'] = pnl['SALES'].pct_change() * 100
+        pnl['NET PROFIT_QoQ'] = pnl['NET PROFIT'].pct_change() * 100
+
+        balancesht = df_comp.loc['BALANCE SHEET'].drop(index='TOTAL', errors='ignore')
+        balancesht.fillna(0, inplace=True)
+        balancesht.index = balancesht.index.str.strip()
+        balancesht = balancesht.transpose()
+        balancesht['WORKING CAPITAL'] = balancesht['OTHER ASSETS'] - balancesht['OTHER LIABILITIES']
+        balancesht['DEBTOR DAYS'] = np.where(pnl['SALES'] > 0, balancesht['RECEIVABLES'] / (pnl['SALES'] / 365), 0)
+        balancesht['INVENTORY TURNOVER'] = np.where(balancesht['INVENTORY'] > 0, pnl['SALES'] / balancesht['INVENTORY'],
+                                                    0)
+        balancesht['ROCE'] = np.where(balancesht['NET BLOCK'] + balancesht['WORKING CAPITAL'] > 0, (
+                    (pnl['OPERATING PROFIT'] - pnl['DEPRECIATION'] - pnl['TAX']) / (
+                        balancesht['NET BLOCK'] + balancesht['WORKING CAPITAL'])) * 100, 0)
+
+        qtr_pnl.fillna(0, inplace=True)
+        qtr_pnl.index = qtr_pnl.index.str.strip()
+        qtr_pnl = qtr_pnl.transpose()
+        qtr_pnl['OPM %'] = qtr_pnl.apply(fundamentals.OPM, axis=1)
+        qtr_pnl['NPM %'] = qtr_pnl.apply(fundamentals.NPM, axis=1)
+        pnl = pnl.transpose()
+        pnl = pnl.round(2)
+        # st.dataframe(pnl)
+        balancesht = balancesht.transpose()
+        balancesht = balancesht.round(2)
+        # st.dataframe(balancesht)
+        qtr_pnl = qtr_pnl.transpose()
+        qtr_pnl = qtr_pnl.round(2)
+        qtr_pnl.columns = qtr_pnl.columns.strftime('%d-%m-%Y')
+        # st.dataframe(qtr_pnl)
         try:
             df_comp.columns = df_comp.columns.strftime('%d-%m-%Y')
         except Exception as AttributeError:
@@ -83,21 +127,21 @@ if uploaded_file is not None:
         with col2:
             sub_choose = st.selectbox("Fundamentals", fundamentals.funda_menu)
         if sub_choose == fundamentals.funda_menu[0]:            # YEARLY PNL
-            index_list = ["key_params"] + list(df_comp.loc[sub_choose].index)
+            index_list = ["key_params"] + list(pnl.index)
             with col3:
                 param = st.selectbox("SubChose", index_list)
             if param == "key_params":
                 with st.expander("YEARLY PROFIT & LOSS DATA"):
-                    st.dataframe(df_comp.loc[sub_choose].style.format(formatter="{:.1f}"))
-                fundamentals.qoq_growth(df_comp.loc[sub_choose], "SALES", color_dict[color_key],comp_Name)
-                fundamentals.group_2_bars(df_comp.loc[sub_choose],"PROFIT BEFORE TAX","NET PROFIT",comp_Name)
+                    st.dataframe(pnl.style.format(formatter="{:.1f}"))
+                fundamentals.qoq_growth(pnl, "SALES", color_dict[color_key],comp_Name)
+                fundamentals.group_2_bars(pnl,"PROFIT BEFORE TAX","NET PROFIT",comp_Name)
             else:
                 with col4:
                     qoq_checked = st.checkbox("Sequential_Growth_%")
                 if qoq_checked:
-                    fundamentals.qoq_growth(df_comp.loc[sub_choose], param, color_dict[color_key],comp_Name)
+                    fundamentals.qoq_growth(pnl, param, color_dict[color_key],comp_Name)
                 else:
-                    fundamentals.go_bar(df_comp.loc[sub_choose], param, color_dict[color_key],comp_Name)
+                    fundamentals.go_bar(pnl, param, color_dict[color_key],comp_Name)
         if sub_choose == fundamentals.funda_menu[3]:                #QUARTERLY PNL
             index_list = ["key_params"] + list(qtr_pnl.index)
             with col3:
