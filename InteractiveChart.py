04adcbbb253e7b84891255c72934d14195c73cec
sahaveer <sahavbachi@gmail.com>
2024-01-14 22:10:45 +0530

    sorted error

--- InteractiveChart.py
+++ InteractiveChart.py
@@ -60,25 +60,25 @@ def get_all_quarterly_list():
             qtr_pnl = pd.read_pickle(f'./pickl/{tree_folder}/{file_name_only}')
             qtr_pnl.columns = pd.to_datetime(qtr_pnl.columns, format='%d-%m-%Y')
 
-            timedelta_90_days = pd.Timedelta(days=90)
+            timedelta_90_days = pd.Timedelta(days=150)
             # st.info(datetime.datetime.now().strptime-qtr_pnl.columns[-1])
             # st.info(type(datetime.datetime.now()-qtr_pnl.columns[-1]))
             if (datetime.datetime.now() - qtr_pnl.columns[-1]) < timedelta_90_days and pickle_name not in latest_quarterly_stocks:
                 latest_quarterly_stocks.append(pickle_name.split()[0])
-                #last_announced_quarter = datetime.datetime.strftime(qtr_pnl.columns[-1],'%b%Y')
+                last_announced_quarter = datetime.datetime.strftime(qtr_pnl.columns[-1],'%b%Y')
             else:
                 latest_quarterly_stocks = "None"
-                #last_announced_quarter = "None"
+                last_announced_quarter = "None"
         if pickle_name.split()[0] not in listed_stocks:
             listed_stocks.append(pickle_name)
-    return listed_stocks, latest_quarterly_stocks
+    return listed_stocks, latest_quarterly_stocks, last_announced_quarter
 
-listed_stocks, latest_quarterly_stocks = get_all_quarterly_list()
+listed_stocks, latest_quarterly_stocks,last_announced_quarter = get_all_quarterly_list()
 
 
 
 st.title(f"👇 Chose among {str(len(listed_stocks))} listed stocks")
-st.subheader(f"{str(len(latest_quarterly_stocks))} stocks announced latest Quarterly Results")
+st.subheader(f"{str(len(latest_quarterly_stocks))} stocks announced {last_announced_quarter} Quarterly Results")
 #selected = st.selectbox("Chose Company", listed_stocks)
 
 # Parse the URL parameters to get the selected stock
