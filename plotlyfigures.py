a7273cb3e47a1db4644f687d6ae52cfa901348fb
sahaveer <sahavbachi@gmail.com>
2025-05-09 17:28:51 +0530

    only from DataB

--- plotlyfigures.py
+++ plotlyfigures.py
@@ -44,23 +44,20 @@ def go_bar(df, row_name,color_bar,comp_Name,filename):
                       bargap=0.15,font_color = "white")     #legend=dict(x=0,y=1.0,bgcolor='rgba(255, 255, 255, 0)',bordercolor='rgba(255, 255, 255, 0)'),
     # new_df = pd.concat([df.loc[row_name]], axis=1).transpose()
     st.plotly_chart(fig,use_container_width=True)
-    col1, col3 = st.columns([2,1])
-    #with col1:
-        #with st.expander(row_name + " DATA"):
-            #st.dataframe(new_df.style.format(formatter="{:.1f}"))
-    with col1:
-        title_text = f"{comp_Name}" # comp_Name.upper() + " " + save_as
-        key_have = f"GoBar {save_as}"
-        description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
-    with col3:
-        if st.button(f'{save_as}.png'):
-            image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-            savenameas = os.path.basename(image_path)
-            print("TRYING TO SAVE IMAGE")
-            fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
-            fig.write_image(image_path, width = 1080, height = 1080)
-            print("Saved as image")
-            instaimage.create_instaimage(title_text,description,image_path)
+    # col1, col3 = st.columns([2,1])
+    # with col1:
+    #     title_text = f"{comp_Name}" # comp_Name.upper() + " " + save_as
+    #     key_have = f"GoBar {save_as}"
+    #     description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
+    # with col3:
+    #     if st.button(f'{save_as}.png'):
+    #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
+    #         savenameas = os.path.basename(image_path)
+    #         print("TRYING TO SAVE IMAGE")
+    #         fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
+    #         fig.write_image(image_path, width = 1080, height = 1080)
+    #         print("Saved as image")
+    #         instaimage.create_instaimage(title_text,description,image_path)
 
     #fig.write_image("./Downloadimages/fig1.png")
     #st.subheader('Downloads:')
@@ -96,68 +93,19 @@ def both_lines(df,row1,row2,color_bar,color_line,comp_Name,filename):
     new_df = new_df.transpose()
 
     col1, col3 = st.columns([1,1])
-    #with col1:
-        #with st.expander(row1 + "/" + row2 + " DATA"):
-            #st.dataframe(new_df.style.format(formatter="{:.1f}"))
+    # with col1:
+    #     title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
+    #     key_have = f"bothlines {save_as}"  
+    #     description = st.text_area(label="👉 Description", value="", height=68,key = key_have)
     # with col3:
-    #     if st.button(f'{save_as}.png'):
+    #     if st.button(f'{save_as}.png', key=save_as):
     #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
     #         savenameas = os.path.basename(image_path)
-    #         # print("TRYING TO SAVE IMAGE")
+    #         print("TRYING TO SAVE IMAGE")
+    #         fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
     #         fig.write_image(image_path, width = 1080, height = 1080)
-    #         # print("Saved as image")
-    with col1:
-        title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
-        key_have = f"bothlines {save_as}"  
-        description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
-    with col3:
-        if st.button(f'{save_as}.png', key=save_as):
-            image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-            savenameas = os.path.basename(image_path)
-            print("TRYING TO SAVE IMAGE")
-            fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
-            fig.write_image(image_path, width = 1080, height = 1080)
-            print("Saved as image")
-            instaimage.create_instaimage(title_text,description,image_path)
-# def bar_line1(df,row1,row2,color_bar,comp_Name,filename):
-#     save_as = f"{filename}  {row1}  {row2}"
-#     save_as = save_as.replace('%','')
-#     dat_rows = [df.loc[row1], df.loc[row2]]
-#     new_df = pd.concat(dat_rows, keys=[row1, row2], axis=1)
-#     # Create figure with secondary y-axis
-#     fig = make_subplots(specs=[[{"secondary_y": False}]])
-#     fig.add_trace(go.Bar(x=new_df.index, y=new_df[row1], name=row1, textposition='auto', text=new_df[row1]),
-#                   secondary_y=False)
-#     fig.update_traces(marker_color=color_bar, marker_line_color='rgb(8,48,107)',
-#                       marker_line_width=1.5, opacity=1, texttemplate='%{text:.3s}', textposition='outside', textfont=dict(size=18), textfont_color='yellow')
-#     fig.add_trace(go.Scatter(x=new_df.index, y=new_df[row2], name=row2, text=new_df[row2]),
-#                   secondary_y=False)
-#     # Add figure title
-#     fig.update_layout(autosize=True, paper_bgcolor="#16181A",plot_bgcolor="#23282D",
-#                       height=height_val,width=width_val,
-#                       margin = dict(l=0,r=0,t=0,b=0, pad=10),
-#                       title={'font':{'color':"#e25f5b"},'text': "<b>" + comp_Name.upper() + "</b> : <i>" + save_as.upper() + '</i>',
-#                              'y': 0.99, 'x': 0.5, 'xanchor': 'right', 'yanchor': 'top'},
-#                       xaxis_tickfont_size=14,
-#                       legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
-#                       bargap=0.15)  # legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01),bargap = 0.15)
-#     # Set y-axes titles
-#     fig.update_yaxes(title_text="<b>" + row1 + "</b>", secondary_y=False, showgrid=False,tickfont=dict(color='white'))
-#     fig.update_yaxes(title_text=write_on_chart + '    ' +"<b>" + row2 + "</b>", secondary_y=False, showgrid=False,tickfont=dict(color='white'))
-#     fig.update_xaxes(tickfont=dict(color='white'),)
-#     st.plotly_chart(fig, use_container_width=True)
-#     new_df = new_df.transpose()
-#     col1, col3 = st.columns([6,4])
-#     #with col1:
-#         #with st.expander(row1 + "/" + row2 + " DATA"):
-#             #st.dataframe(new_df.style.format(formatter="{:.1f}"))
-#     with col3:
-#         if st.button(f'{save_as}.png'):
-#             image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-#             savenameas = os.path.basename(image_path)
-#             print("TRYING TO SAVE IMAGE")
-#             fig.write_image(image_path, width = 1080, height = 1080)
-#             print("Saved as image")
+    #         print("Saved as image")
+    #         instaimage.create_instaimage(title_text,description,image_path)
 
 def bar_line(df,row1,row2,color_bar,comp_Name,filename):
     save_as = f"{filename} {row1} {row2}"
@@ -188,29 +136,19 @@ def bar_line(df,row1,row2,color_bar,comp_Name,filename):
     st.plotly_chart(fig, use_container_width=True)
     new_df = new_df.transpose()
     col1, col3 = st.columns([2,1])
-    #with col1:
-        #with st.expander(row1 + "/" + row2 + " DATA"):
-            #st.dataframe(new_df.style.format(formatter="{:.1f}"))
+    # with col1:
+    #     title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
+    #     key_have = f"barline {save_as} "
+    #     description = st.text_area(label="👉 Description", value="", height=68,key = key_have)
     # with col3:
-    #     if st.button(f'{save_as}.png'):
+    #     if st.button(f'{save_as}.png', key=save_as):
     #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
     #         savenameas = os.path.basename(image_path)
     #         print("TRYING TO SAVE IMAGE")
+    #         fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
     #         fig.write_image(image_path, width = 1080, height = 1080)
     #         print("Saved as image")
-    with col1:
-        title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
-        key_have = f"barline {save_as} "
-        description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
-    with col3:
-        if st.button(f'{save_as}.png', key=save_as):
-            image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-            savenameas = os.path.basename(image_path)
-            print("TRYING TO SAVE IMAGE")
-            fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
-            fig.write_image(image_path, width = 1080, height = 1080)
-            print("Saved as image")
-            instaimage.create_instaimage(title_text,description,image_path)
+    #         instaimage.create_instaimage(title_text,description,image_path)
 
 def qoq_growth(df,row_name,color_bar,comp_Name,filename):
     save_as = f"{filename}  {row_name}".upper()
@@ -258,129 +196,25 @@ def qoq_growth(df,row_name,color_bar,comp_Name,filename):
     #fig.update_layout(annotations=[dict(text=write_annotation,x=0,y=1,xref='paper',yref='paper',showarrow=False,font=dict(size=18, color=color_bar))])
     st.plotly_chart(fig,use_container_width=True)
 
-    col1, col3 = st.columns([2,1])
-    #with col1:
-        # title_text = st.text_area(key="TITLE", label="Title Text", value=comp_Name.upper() + " Q2FY24", height=50)
-        #subject_text = st.text_area(label="Edit to create subject line in instagram image", value=write_annotation,height=25)
-        #with st.expander(row_name + " DATA"):
-            #st.dataframe(new_df.style.format(formatter="{:.1f}"))
-    with col1:
-        title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
-        key_have = f"qoq {save_as} "
-        description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
-    with col3:
-        if st.button(f'{save_as}.png'):
-            image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-            savenameas = os.path.basename(image_path)
-            print("TRYING TO SAVE IMAGE")
-            fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
-            fig.write_image(image_path, width = 1080, height = 1080)
-            print("Saved as image")
-            instaimage.create_instaimage(title_text,description,image_path)
-
+    # col1, col3 = st.columns([2,1])
+    # with col1:
+    #     title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
+    #     key_have = f"qoq {save_as} "
+    #     description = st.text_area(label="👉 Description", value="", height=68,key = key_have)
     # with col3:
     #     if st.button(f'{save_as}.png'):
-    #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as + ".png"
+    #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
     #         savenameas = os.path.basename(image_path)
-    #         fig.update_layout(autosize=False, paper_bgcolor="#16181A",plot_bgcolor="#23282D",)
+    #         print("TRYING TO SAVE IMAGE")
+    #         fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
     #         fig.write_image(image_path, width = 1080, height = 1080)
-    #         #(15,186,236)    (235,202,10)
-    #         #Candara arial
-    #         title_font = ImageFont.truetype("Candara.ttf", size=96)
-    #         subject_font = ImageFont.truetype("arial.ttf", size=42)
-    #         title_color = color_dict['black']['rgb']
-    #         title_box_color = color_bar # color_dict['blue1']['rgb']
-    #         subject_box_color = title_color
-    #         subject_color = color_bar
-    #
-    #         if os.path.exists(image_path):
-    #             instaimage.create_image(centre_image=image_path, title_text=comp_Name.upper() + " Q2FY24", title_font=title_font, title_color=title_color, title_box_color=title_box_color,
-    #                                     subject_text=subject_text, subject_font=subject_font ,subject_color=subject_color, subject_box_color = subject_box_color,
-    #                                     output_path= TARGET_FOLDER + save_as + ".png")  # send the path of Centre image not the Image itself
-
-            #print("Saved as image")
-            #with open(image_path, "rb") as file:
-                #btn = st.download_button(label="Download",data=file,file_name=savenameas,mime="image/png")
+    #         print("Saved as image")
+    #         instaimage.create_instaimage(title_text,description,image_path)
 
     #st.subheader('Downloads:')
     #excel_link_to_download(df2)
     #generate_html_Download_link(fig)
 
-# def qoq_growth1(df,color_bar,comp_Name,filename):
-#     save_as = f"{filename}  {row_name}".upper()
-#     col1_chart, col2_chart = st.columns([3, 8])
-#     index_list = list(df.index)
-#     with col1_chart:
-#         row_name = option_menu("", index_list, default_index=0, orientation="vertical")
-#     temp_df = df.loc[row_name]
-#     df_qoq = (temp_df.pct_change() * 100)
-#     df_qoq.name = row_name + '_QoQ'
-#     df_qoq = pd.DataFrame(df_qoq).transpose()
-#     df2 = pd.concat([df, df_qoq], axis=0)
-#     # Create figure with secondary y-axis
-#     fig = make_subplots(specs=[[{"secondary_y": True}]])
-#     # adds bar chart
-#     fig.add_trace(go.Bar(x=df2.columns, y=df2.loc[row_name], name=row_name, textposition = 'auto',text = df.loc[row_name]),
-#                    secondary_y=False)
-#     fig.update_traces(marker_color=color_bar, marker_line_color='rgb(8,48,107)',
-#                       marker_line_width=1.5, opacity=1, texttemplate='%{text:.3s}', textposition='outside', textfont=dict(size=18), textfont_color='yellow')
-#     # adds line chart
-#     fig.add_trace(go.Scatter(x=df2.columns, y=df2.iloc[-1], name=row_name + " QoQ", text = df.iloc[-1]),
-#                    secondary_y=True)
-#     # Add figure title
-#     fig.update_layout(autosize=True, paper_bgcolor="#16181A",plot_bgcolor="#23282D",
-#                       height=height_val, width=width_val,
-#                       margin=dict(l=0, r=0, t=0, b=0, pad=10),
-#                       title={'font':{'color':"#e25f5b"},'text': "<b>" + comp_Name.upper() + "</b> : <i>" + save_as + '</i>',
-#                               'y': 0.99, 'x': 0.5, 'xanchor': 'right', 'yanchor': 'top'},
-#                        xaxis_tickfont_size=14,
-#                        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
-#                        bargap=0.15)#legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01),bargap = 0.15)
-#     # Set y-axes titles
-#     fig.update_yaxes(title_text=write_on_chart + '    ' +"<b>" + row_name + "</b> in cr ", secondary_y=False,showgrid=False,tickfont=dict(color='white'))
-#     fig.update_yaxes(title_text="<b> QoQ </b> in % ", secondary_y=True,showgrid=False,tickfont=dict(color='white'))
-#
-#     # Set x-axis tick color and font color
-#     fig.update_xaxes(tickfont_color='white')  # Set font color for x-axis tick labels
-#
-#     new_df = pd.concat([df2.loc[row_name],df2.iloc[-1]], axis=1).transpose()
-#     latest_sales = new_df.iloc[0, -1]
-#     qoq = new_df.iloc[1,-1]
-#     if qoq > 0:
-#         write_annotation = f"{comp_Name} has clocked Revenue of {latest_sales:.1f}cr up by {qoq:.1f}% with the previous Q1FY24"
-#     else:
-#         write_annotation =   f"{comp_Name} has clocked Revenue of {latest_sales:.1f}cr down by {qoq:.1f}% with the previous Q1FY24"
-#     #fig.add_annotation(text=write_annotation, x=0, y=1, xref="paper", yref="paper",showarrow=False, font=dict(size=18, color=color_bar))
-#     #fig.update_layout(annotations=[dict(text=write_annotation,x=0,y=1,xref='paper',yref='paper',showarrow=False,font=dict(size=18, color=color_bar))])
-#     with col2_chart:
-#         st.plotly_chart(fig,use_container_width=True)
-#
-#     col1, col3 = st.columns([6,4])
-#     #with col1:
-#         # title_text = st.text_area(key="TITLE", label="Title Text", value=comp_Name.upper() + " Q2FY24", height=50)
-#         #subject_text = st.text_area(label="Edit to create subject line in instagram image", value=write_annotation,height=25)
-#         #with st.expander(row_name + " DATA"):
-#             #st.dataframe(new_df.style.format(formatter="{:.1f}"))
-#     with col3:
-#         if st.button(f'{save_as}.png'):
-#             image_path = './Downloadimages/' + comp_Name.upper() + " " + row_name + ".png"
-#             savenameas = os.path.basename(image_path)
-#             #print("TRYING TO SAVE IMAGE")
-#             fig.write_image(image_path, width = 1080, height = 1080)
-#             #(15,186,236)    (235,202,10)
-#             if os.path.exists(image_path):
-#                 instaimage.create_image(centre_image=image_path, title_text=comp_Name.upper() + " Q2FY24", title_color=(15,186,236),  subject_text=subject_text, subject_color=(255,255,255),
-#                              output_path= TARGET_FOLDER + save_as + ".png")  # send the path of Centre image not the Image itself
-#
-#             #print("Saved as image")
-#             #with open(image_path, "rb") as file:
-#                 #btn = st.download_button(label="Download",data=file,file_name=savenameas,mime="image/png")
-#
-#     #st.subheader('Downloads:')
-#     #excel_link_to_download(df2)
-#     #generate_html_Download_link(fig)
-#this has 2 series concatenated, these 2 are shown as GroupBar
-#not in use
 
 #not in use
 def peer_bar(df,Name,comp1_Name,comp2_Name):   #this has 2 series concatinated with key names
@@ -556,19 +390,19 @@ def group_2_bars(df,row1,row2,comp_Name,filename):
     #         print("TRYING TO SAVE IMAGE")
     #         fig.write_image(image_path, width = 1080, height = 1080)
     #         print("Saved as image")
-    with col1:
-        title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
-        key_have = f"Group2bars {save_as}"
-        description = st.text_area(label="👉 Description", value="", height=15,key = key_have)
-    with col3:
-        if st.button(f'{save_as}.png', key=save_as):
-            image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
-            savenameas = os.path.basename(image_path)
-            print("TRYING TO SAVE IMAGE")
-            fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
-            fig.write_image(image_path, width = 1080, height = 1080)
-            print("Saved as image")
-            instaimage.create_instaimage(title_text,description,image_path)
+    # with col1:
+    #     title_text = f"{comp_Name}" #comp_Name.upper() + " " + save_as
+    #     key_have = f"Group2bars {save_as}"
+    #     description = st.text_area(label="👉 Description", value="", height=68,key = key_have)
+    # with col3:
+    #     if st.button(f'{save_as}.png', key=save_as):
+    #         image_path = './Downloadimages/' + comp_Name.upper() + " " + save_as.upper() + ".png"
+    #         savenameas = os.path.basename(image_path)
+    #         print("TRYING TO SAVE IMAGE")
+    #         fig.update_layout(autosize=True, paper_bgcolor="#16181A", plot_bgcolor="#23282D",)
+    #         fig.write_image(image_path, width = 1080, height = 1080)
+    #         print("Saved as image")
+    #         instaimage.create_instaimage(title_text,description,image_path)
 
 # THIS IS SPECIFICALLY DESIGNED FOR CASHFLOWs, Where fixed 4 rows are there
 def go_group_bar(df, row_name,color_bar,filename):
